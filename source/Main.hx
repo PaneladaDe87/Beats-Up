cc9694a5d404c471e8d7774d5ee5130d94b5900e
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-05-11 21:18:11 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,10 +1,24 @@
 import openfl.display.Sprite;
+import openfl.display.Graphics;
 import openfl.display.StageScaleMode;
 import openfl.display.StageAlign;
 
 class Main extends Sprite {
-    public function start() {
+    public var Color:Int = 0x000000;
+    
+    public function Start() {
+        super();
         stage.scaleMode = StageScaleMode.EXACT_FIT;
         stage.align = StageAlign.TOP_LEFT;
+        
+        Load();
+    }
+    
+    private function Load():Void {
+        var G:Graphics = graphics;
+            
+        G.beginFill(Color);
+        G.drawRect(0, 0, stage.stageWidth, stage.stageHeight);
+        G.endFill();
     }
 }
